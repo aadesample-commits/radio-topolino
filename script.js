332e9dacd91c7b6ca80cb7c421a168f30880ceaf
aadesample-commits <aadesample@gmail.com>
2026-04-08 19:11:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,28 @@ const userNameInput = document.getElementById('user-name');
 
 // 1. Lista canzoni e durate (MODIFICA QUI CON I TUOI FILE)
 const songs = [
-    { name: "Canzone 1", url: "musica/nome_file_1.mp3", durata: 180 },
-    { name: "Canzone 2", url: "musica/nome_file_2.mp3", durata: 210 }
+    { name: "Canzone 1", url: "musica/A$AP Rocky - Everyday (Official Audio) ft. Rod Stewart, Miguel, Mark Ronson.mp3", durata: 262 },
+    { name: "Canzone 2", url: "musica/Rapp Snitch Knishes (feat. Mr. Fantastik).mp3", durata: 172 },
+    { name: "Canzone 3", url: "musica/Flashing Lights.mp3", durata: 237 },
+    { name: "Canzone 4", url: "musica/Father Stretch My Hands Pt. 1.mp3", durata: 135 },
+    { name: "Canzone 5", url: "musica/No Role Modelz.mp3", durata: 293 },
+    { name: "Canzone 6", url: "musica/It Was A Good Day.mp3", durata: 260 },
+    { name: "Canzone 7", url: "musica/Drake - Passionfruit.mp3", durata: 299 },
+    { name: "Canzone 8", url: "musica/Bob Marley - Could You Be Loved (HQ).mp3", durata: 235 },
+    { name: "Canzone 9", url: "musica/Drake - Time Flies (Lyrics).mp3", durata: 192 },
+    { name: "Canzone 10", url: "musica/Nirvana - Smells Like Teen Spirit (Official Music Video).mp3", durata: 278 },
+    { name: "Canzone 11", url: "musica/Ray Charles - Hit The Road Jack (Official Lyrics Video).mp3", durata: 122 },
+    { name: "Canzone 12", url: "musica/CULO.mp3", durata: 177 },
+    { name: "Canzone 13", url: "musica/DONNE RICCHE - TonyPitony | ACOUSTIC VERSION.mp3", durata: 172 },
+    { name: "Canzone 14", url: "musica/STRISCIA.mp3", durata: 205 },
+    { name: "Canzone 15", url: "musica/Fabri Fibra-Stavo pensando a te.mp3", durata: 266 },
+
+
+
+
+
+
+
 ];
 
 // 2. Funzione di Sincronizzazione
